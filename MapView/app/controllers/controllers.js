1a846c64d11f10d5f56372315cc8bc6b3ea7beba
Adam Zieliński <zielinski.adam1206@gmail.com>
2017-05-14 15:14:04 +0200

    dodanie drag and drop

--- MapView/app/controllers/controllers.js
+++ MapView/app/controllers/controllers.js
@@ -2,7 +2,7 @@
  * Created by Adam Zieliński on 2017-05-13.
  */
 
-var MyFirstController = function ($scope, $http, userData, userGravatar, gitHubUserLookup) {
+var MyFirstController = function ($scope, $http, $q, userData, userGravatar, gitHubUserLookup, placeService) {
     $scope.ManyHellos = ['Hello', 'Hola', 'Bonjour', 'Guten Tag', 'Ciao', 'Namaste', 'Yiasou'];
 
     $scope.data = userData.user;
@@ -75,6 +75,81 @@ var MyFirstController = function ($scope, $http, userData, userGravatar, gitHubU
         strokeWeight: 0.4
     };
 
+    $scope.models = [
+        {listName: "A", items: [], dragging: false},
+        {listName: "B", items: [], dragging: false}
+    ];
+
+    var promise = placeService.getPlacesType();
+    promise.then(function (data) {
+            $scope.places= data.data.placeTypes;
+            console.log($scope.places);
+        }
+    )
+
+    /**
+     * dnd-dragging determines what data gets serialized and send to the receiver
+     * of the drop. While we usually just send a single object, we send the array
+     * of all selected items here.
+     */
+    $scope.getSelectedItemsIncluding = function (list, item) {
+        item.selected = true;
+        return list.items.filter(function (item) {
+            return item.selected;
+        });
+    };
+
+    /**
+     * We set the list into dragging state, meaning the items that are being
+     * dragged are hidden. We also use the HTML5 API directly to set a custom
+     * image, since otherwise only the one item that the user actually dragged
+     * would be shown as drag image.
+     */
+    $scope.onDragstart = function (list, event) {
+        list.dragging = true;
+        if (event.dataTransfer.setDragImage) {
+            var img = new Image();
+            img.src = 'framework/vendor/ic_content_copy_black_24dp_2x.png';
+            event.dataTransfer.setDragImage(img, 0, 0);
+        }
+    };
+
+    /**
+     * In the dnd-drop callback, we now have to handle the data array that we
+     * sent above. We handle the insertion into the list ourselves. By returning
+     * true, the dnd-list directive won't do the insertion itself.
+     */
+    $scope.onDrop = function (list, items, index) {
+        angular.forEach(items, function (item) {
+            item.selected = false;
+        });
+        list.items = list.items.slice(0, index)
+            .concat(items)
+            .concat(list.items.slice(index));
+        return true;
+    }
+
+    /**
+     * Last but not least, we have to remove the previously dragged items in the
+     * dnd-moved callback.
+     */
+    $scope.onMoved = function (list) {
+        list.items = list.items.filter(function (item) {
+            return !item.selected;
+        });
+    };
+
+    // Generate the initial model
+    angular.forEach($scope.models, function (list) {
+        for (var i = 1; i <= 4; ++i) {
+            list.items.push({label: "Item " + list.listName + i});
+        }
+    });
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function (model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
 
     $scope.initMap = function () {
         if ($scope.initialized == false) {
@@ -92,7 +167,7 @@ var MyFirstController = function ($scope, $http, userData, userGravatar, gitHubU
             service.nearbySearch({
                 location: $scope.startPoint,
                 radius: 500,
-                type: ['bus_station']
+                type: ['store']
             }, $scope.processResults);
         }
     }
