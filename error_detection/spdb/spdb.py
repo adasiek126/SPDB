09eb6660b48ce68e42bc8aa0ed7f8a8c03e9d88a
Kamil Bachanek <Kamil Bachanek>
2017-05-14 17:13:39 +0200

    Added support fot UTF-8

--- error_detection/spdb/spdb.py
+++ error_detection/spdb/spdb.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import codecs
+
 app = Flask(__name__)
 pageContent = open('main.html', 'rb').read().decode('utf-8')
 
@@ -8,8 +10,9 @@ def index():
 
 @app.route('/api/getBadBusStops/', methods=['GET'])
 def getBadBusStops():
-    default = open("default.json", "r")
-    return default.read()
+    with codecs.open("default.json",'r',encoding='utf8') as f:
+        text = f.read()
+    return text
 
 if __name__ == '__main__':
    app.run()
